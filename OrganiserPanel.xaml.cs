fdb20fc6dfab8618fa5d2391a59f17030699c195
hunor64 <nhunor06@gmail.com>
2024-10-03 14:21:55 +0200

    Added organiser panel

--- OrganiserPanel.xaml.cs
+++ OrganiserPanel.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,33 @@ namespace SportFogadas
         public OrganiserPanel()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection("Server=localhost;Database=Bets;Uid=root;Pwd=;");
+            connection.Open();
+            string query = "INSERT INTO Events (EventName, EventDate, Category, Location) VALUES (@EventName, @EventDate, @Category, @Location)";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@EventName", txbEventName);
+                command.Parameters.AddWithValue("@EventDate", dtpEventDate);
+                command.Parameters.AddWithValue("@Category", txbCategory);
+                command.Parameters.AddWithValue("@Location", txbLocation);
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Event created successfully!");
+                    this.DialogResult = true;
+                    connection.Close();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to create event!");
+                }
+            }
         }
     }
 }
