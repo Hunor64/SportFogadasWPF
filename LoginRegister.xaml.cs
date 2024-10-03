a3dd96b419824c45bb7549767afe388de1ceef0a
hunor64 <nhunor06@gmail.com>
2024-10-03 09:42:31 +0200

    Added basic login/register window

---
+++ LoginRegister.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SportFogadas
+{
+    /// <summary>
+    /// Interaction logic for LoginRegister.xaml
+    /// </summary>
+    public partial class LoginRegister : Window
+    {
+        public LoginRegister(string userName)
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
