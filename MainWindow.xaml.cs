e12f5ce26c03839adb4aadec4779c1bf0558d4fd
hunor64 <nhunor06@gmail.com>
2024-10-03 10:37:43 +0200

    Added db commands, Added regions to main window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Google.Protobuf.WellKnownTypes;
 using MySql.Data.MySqlClient;
 
 namespace SportFogadas
@@ -19,43 +20,80 @@ namespace SportFogadas
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Initial Variables
         private DebugWindow debugWindow;
         public bool debugOn = true;
+        MySqlConnection connection = new MySqlConnection("Server=localhost;Database=Bets;Uid=root;Pwd=;");
+        #endregion
 
         public MainWindow()
         {
+            #region Initialize Windows
             InitializeComponent();
-            debugWindow = new DebugWindow(this,debugOn);
+            debugWindow = new DebugWindow(this, debugOn);
             debugWindow.Show();
-            ConnectToMySql();
+            #endregion
+
+            #region Database Connection
+
+            try
+            {
+                connection.Open();
+                debugWindow.Write("Connected to MySQL Server");
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting to MySQL Server: " + ex.Message);
+                debugWindow.Write("Error connecting to MySQL Server: " + ex.Message);
+                this.Close();
+            }
+
+            #endregion
+
         }
 
-        public void ConnectToMySql()
+        #region Database Functions
+        public void Exec(string command)
         {
-            string connectionString = "Server=localhost;Database=Bets;Uid=root;Pwd=;";
-
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            try
             {
-                try
-                {
-                    connection.Open();
-                    debugWindow.Write("Connected to MySQL Server");
-                    debugWindow.Write("Connection String: " + connectionString);
-                    
-                    connection.Close();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error connecting to MySQL Server: " + ex.Message);
-                    debugWindow.Write("Error connecting to MySQL Server: " + ex.Message);
-                    this.Close();
-                }
+                MySqlCommand cmd = new MySqlCommand(command, connection);
+                cmd.ExecuteNonQuery();
+                debugWindow.Write("Command executed successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error executing command: " + ex.Message);
+                debugWindow.Write("Error executing command: " + ex.Message);
             }
         }
+        public MySql.Data.MySqlClient.MySqlDataReader ReadDB(string command)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(command, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                debugWindow.Write("Command executed successfully");
+                return reader;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error executing command: " + ex.Message);
+                debugWindow.Write("Error executing command: " + ex.Message);
+                return null;
+            }
+        }
+        #endregion
+
+        #region Event Handlers
         protected override void OnClosed(EventArgs e)
         {
+            connection.Close();
             base.OnClosed(e);
             debugWindow.Close();
+
         }
+        #endregion
     }
 }
